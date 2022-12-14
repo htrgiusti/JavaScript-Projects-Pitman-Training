63c2599c8a01b11d1158b4733b40773d31edf464
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2022-12-14 03:21:17 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date ()
+}
+
+function myFunction() { //Creates a function called "myFunction".
+    var sentence = "I am learning"; //Creates a string variable.
+    sentence += " a lot from this book!";//Creates another string variable with += to concatenate the text.
+    document.getElementById("Concatenate").innerHTML = sentence;//this method gets the id "Concatenate" in the HTML file. 
+}
+
