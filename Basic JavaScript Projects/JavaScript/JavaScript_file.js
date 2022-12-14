8b168762c23d1494cb3805a8dfde5215b262a041
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2022-12-14 02:39:11 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/JavaScript/JavaScript_file.js
@@ -0,0 +1,11 @@
+window.alert("Hi!"); //This will pop up a window with this string inside
+
+var B = "Blue", R = "Red"; //These are variables B for Blue and R for Red
+var B = B.fontcolor("Blue"), R = R.fontcolor("Red"); //This will change the variable string color
+
+document.write(R+B); //This will write the strings in the variables
+
+document.write(4*4); //This is an expression and It will execute the math operation 4x4
+
+
+
