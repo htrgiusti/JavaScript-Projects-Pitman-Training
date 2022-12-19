ec9e2f96ee56b13e31c254b48e214e649f4824bb
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2022-12-19 11:18:15 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/Project4_dictionaries/JavaScript/main.js
@@ -0,0 +1,8 @@
+function my_Dictionary () {
+    var Animal = {
+        Species: "Dog",
+        Breed: "Dachshund",
+        Sound: "Bark!"
+    };
+    document.getElementById("Dictionary").innerHTML= Animal.Sound;
+}
