0e1611c9d288620d744c701995f371b3a10c5c87
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2022-12-19 11:46:04 +0000

    Delete Basic JavaScript Projects/Project4_dictionaries directory

--- Basic JavaScript Projects/Project4_dictionaries/JavaScript/main.js
+++
@@ -1,8 +0,0 @@
-function my_Dictionary () {
-    var Animal = {
-        Species: "Dog",
-        Breed: "Dachshund",
-        Sound: "Bark!"
-    };
-    document.getElementById("Dictionary").innerHTML= Animal.Sound;
-}
